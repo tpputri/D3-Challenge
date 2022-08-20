e1a8593a38e637e5eda37f320bf95138ff453ad7
tuflikha putri <tuflikha.putri@y7mail.com>
2022-08-20 21:28:31 +1000

    fixing data loading issue

--- js/app.js
+++ js/app.js
@@ -114,7 +114,7 @@ function updateToolTip(chosenXAxis, chosenYAxis, circlesGroup) {
 }
 
 // Retrieve data from the CSV file and execute everything below
-d3.csv("data/data.csv").then(function (D3Data, err) {
+d3.csv("data-source/data.csv").then(function (D3Data, err) {
   if (err) throw err;
 
   var i = 3;
